6ab31d84bc6c68f1573fb7d228fd4aabd484658e
Enoma Deborah <enomaomorogieva@gmail.com>
2019-09-01 13:34:04 +0100

    Fix sign in issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for
+  def after_sign_in_path_for(resource_or_scope)
     events_path
   end
 
-  def after_update_path_for
+  def after_update_path_for(resource)
     events_path
   end
 end
